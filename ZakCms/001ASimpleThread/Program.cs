063947c3cd8ea7e9576a96ff673e4475671949ad
endaroza <github@kendar.org>
2012-10-29 09:04:07 +0100

    Added first sample

---
+++ ZakCms/001ASimpleThread/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using ZakThread.Threading.Enums;
+
+namespace _001_A_simple_thread
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var stopwatch = new Stopwatch();
+			var stopwatchExternal = new Stopwatch();
+			var testThread = new TestThread(stopwatch, "TestThread");
+
+			testThread.RunThread();
+			stopwatchExternal.Start();
+			while (testThread.Status != RunningStatus.Halted)
+			{
+				Thread.Sleep(5);
+			}
+			stopwatchExternal.Stop();
+			Console.WriteLine(
+				string.Format(
+					"Completed in {0} ms, lifecycle was of {1} ms.",
+					stopwatch.ElapsedMilliseconds,
+					stopwatchExternal.ElapsedMilliseconds));
+			//Just to avoid the app closing...
+			Console.ReadKey();
+		}
+	}
+}
---
+++ ZakCms/001ASimpleThread/TestThread.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics;
+using ZakThread.Logging;
+using ZakThread.Threading;
+
+namespace _001_A_simple_thread
+{
+	public class TestThread : BaseThread
+	{
+		private readonly Stopwatch _stopwatch;
+		private int _counter;
+		private const int MAX_COUNT = 100;
+
+		public TestThread(Stopwatch stopwatch, string threadName, bool restartOnError = true) :
+			base(NullLogger.Create(), threadName, restartOnError)
+		{
+			_stopwatch = stopwatch;
+			_counter = 0;
+		}
+
+		//Overridden, returns true when all goes well
+		protected override bool RunSingleCycle()
+		{
+			if (_counter == 0) _stopwatch.Start();
+			Console.WriteLine(string.Format("Executing thread {0} for the {1}th time.", ThreadId, _counter));
+			_counter++;
+			if (_counter >= MAX_COUNT)
+			{
+				_stopwatch.Stop();
+				Terminate();
+			}
+			return true;
+		}
+	}
+}
