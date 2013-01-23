ae5a7c8f9f42babda7476f260241c4e0fc76208c
endaroza <github@kendar.org>
2013-01-23 17:37:01 +0100

    Updated versions for breaking change before nuget

--- ZakFramework/ZakThread/Properties/AssemblyInfo.cs
+++ ZakFramework/ZakThread/Properties/AssemblyInfo.cs
@@ -36,8 +36,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
+[assembly: AssemblyFileVersion("2.0.1.0")]
 
 // For unit tests
 [assembly: InternalsVisibleTo("ZakThread.Test")]
