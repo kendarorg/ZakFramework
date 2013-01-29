e692ba772b1f4d60342f70543563c3671e3b9492
endaroza <github@kendar.org>
2013-01-29 12:30:44 +0100

    Added db interfaces
    And back to 78% coverage

---
+++ ZakFramework/ZakDb.Test/DbTest.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using ZakDb.Plugins;
+
+namespace ZakDb.Test
+{
+	[TestFixture]
+	public class DbTest
+	{
+			[Test]
+			public void ItShouldBePossibleToCreateAPluginOutcome()
+			{
+				var po = new PluginOutcome();
+				Assert.IsFalse(po.Success);
+				Assert.IsNull(po.Result);
+			}
+	}
+}
